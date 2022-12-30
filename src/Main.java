f9bd78cb526fe24b0e963366b791620539f12b65
Grisha Petrosyan <petrosyan.grigorij2018@yandex.ru>
2022-12-30 14:44:12 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int milePrice = 20;
+        int mile = ticketPrice / milePrice;
+        System.out.println((mile) + "количество миль начисленных за купленный билет");
+    }
+}
